9b177b812ae6f8aedd3d794b55171a4b2d74a98d
rosemutai <chepngetichrose2030@gmail.com>
2023-01-09 20:25:24 +0300

    add Navbar and homepage components

---
+++ catwiki/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import HomePage from './components/HomePage';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className='app w-full min-h-screen pt-8 px-8'>
+      <Navbar />
+      <HomePage />
+      
+    </div>
+  );
+}
+
+export default App;
