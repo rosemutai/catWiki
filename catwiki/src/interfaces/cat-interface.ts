b4ed8076db67edbba48dc2c5c2dd71adc54d3aef
rosemutai <chepngetichrose2030@gmail.com>
2023-01-10 21:19:23 +0300

    cat interface

---
+++ catwiki/src/interfaces/cat-interface.ts
@@ -0,0 +1,10 @@
+export type Cat = {
+    name: string,
+    cfa_url: string,
+    vcahospitals_url: string,
+    temperament: string[],
+    origin: string,
+    description: string,
+    life_span: string,
+    wikipedia_url: string
+}
