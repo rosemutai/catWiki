cc6eab8d97535b730bcb6d4441beacee8fb16c36
rosemutai <chepngetichrose2030@gmail.com>
2023-01-11 20:10:16 +0300

    add breed details

--- catwiki/src/components/BreedDetails.tsx
+++ catwiki/src/components/BreedDetails.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Cat } from '../interfaces/cat-interface'
 
-const BreedDetails = () => {
+type CatProps = {
+  catBreed: Cat
+}
+
+const BreedDetails = ({catBreed}: CatProps) => {
   return (
-    <div>
+    <div className='cat-details w-full rounded-2xl bg-light-grey'>
+      <img className='w-16 h-16 rounded-full' src={catBreed.image_link} alt={catBreed.name} />
+      <div className="details text-center text-brownish">
+          <h3>{catBreed.name}</h3>
+          <p>{catBreed.origin}</p>
+
+      </div>
+      
         
     </div>
   )
