5750073a2bf8f42c25347860f480691a852a3c53
rosemutai <chepngetichrose2030@gmail.com>
2023-01-12 18:45:59 +0300

    remove unused variables

--- catwiki/src/components/HomePage.tsx
+++ catwiki/src/components/HomePage.tsx
@@ -1,5 +1,4 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
+import React, { useState} from 'react'
 import Breeds from './Breeds'
 import Footer from './Footer'
 import Header from './Header'
